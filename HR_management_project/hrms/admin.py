44bd089e5fbeb6a3ae6ef47afa5beceb1340ad14
Alain Yankap <alainyankap@gmail.com>
2026-05-04 18:45:13 -0600

    fAdded models in admin

--- HR_management_project/hrms/admin.py
+++ HR_management_project/hrms/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Department, Employee, Project
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Project)
